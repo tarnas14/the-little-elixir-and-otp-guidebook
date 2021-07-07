576b4eb92bad67756538b6a016db7ac8aa9df9df
tarnas14 <tarnas14@gmail.com>
2021-07-07 19:56:26 +0200

    correctly handling worker exit via monitors

--- pooly/lib/pool_server.ex
+++ pooly/lib/pool_server.ex
@@ -6,9 +6,11 @@ defmodule Pooly.PoolServer do
     defstruct pool_sup: nil,
               worker_sup: nil,
               size: nil,
-              workers: nil, # available workers
+              # available workers
+              workers: nil,
               imfa: nil,
-              monitors: nil, # monitored client processes, client references by worker id
+              # monitored client processes, client references by worker id
+              monitors: nil,
               name: nil,
               overflow: nil,
               max_overflow: nil,
@@ -46,13 +48,13 @@ defmodule Pooly.PoolServer do
     } = state
 
     case workers do
-      [worker | rest] ->
+      [{worker, _worker_ref} | rest] ->
         ref = Process.monitor(from_pid)
         true = :ets.insert(monitors, {worker, ref})
         {:reply, worker, %{state | workers: rest}}
 
       [] when max_overflow > 0 and overflow < max_overflow ->
-        worker = new_worker(worker_sup, imfa)
+        {worker, _worker_ref} = new_worker(worker_sup, imfa)
         ref = Process.monitor(from_pid)
         true = :ets.insert(monitors, {worker, ref})
         {:reply, worker, %{state | overflow: overflow + 1}}
@@ -107,37 +109,41 @@ defmodule Pooly.PoolServer do
     end
   end
 
-  # handling consumer down message
-  def handle_info({:DOWN, ref, _, _, _}, state = %{monitors: monitors, workers: workers}) do
+  def handle_info(
+        {:DOWN, ref, :process, downed_pid, _},
+        state = %{monitors: monitors, workers: workers}
+      ) do
     case :ets.match(monitors, {:"$1", ref}) do
+      # handling consumer exit
       [[pid]] ->
         true = :ets.delete(monitors, pid)
         new_state = %{state | workers: [pid | workers]}
         {:noreply, new_state}
 
-      [[]] ->
+      # handling worker exit
+      [] when is_pid(downed_pid) ->
+        case :ets.lookup(monitors, downed_pid) do
+          [{pid, ref}] ->
+            true = Process.demonitor(ref)
+            true = :ets.delete(monitors, downed_pid)
+            new_state = handle_worker_exit(downed_pid, state)
+            {:noreply, new_state}
+
+          [] ->
+            new_state = handle_worker_exit(downed_pid, state)
+            {:noreply, new_state}
+
+          _ ->
+            {:noreply, state}
+        end
+
+      _ ->
         {:noreply, state}
     end
   end
 
   def handle_info({:EXIT, worker_sup, reason}, state = %{worker_sup: worker_sup}) do
     {:stop, reason, state}
-  end
-
-  def handle_info(
-        {:EXIT, pid, _reason},
-        state = %{monitors: monitors}
-      ) do
-    case :ets.lookup(monitors, pid) do
-      [{pid, ref}] ->
-        true = Process.demonitor(ref)
-        true = :ets.delete(monitors, pid)
-        new_state = handle_worker_exit(pid, state)
-        {:noreply, new_state}
-
-      [[]] ->
-        {:noreply, state}
-    end
   end
 
   def init([pool_sup, pool_config]) when is_pid(pool_sup) do
@@ -181,12 +187,6 @@ defmodule Pooly.PoolServer do
   defp supervisor_spec(%{name: name, imfa: imfa}) do
     opts = [id: name <> "WorkerSupervisor", restart: :temporary]
     supervisor(Pooly.WorkerSupervisor, [self, imfa], opts)
-    # %{
-      # id: name <> "WorkerSupervisor",
-      # start: {Pooly.WorkerSupervisor, :start_link, [self]},
-      # restart: :temporary,
-      # type: :supervisor
-    # }
   end
 
   defp prepopulate(size, worker_sup, imfa) do
@@ -204,7 +204,8 @@ defmodule Pooly.PoolServer do
   defp new_worker(worker_sup, imfa) do
     case Supervisor.start_child(worker_sup, [[]]) do
       {:ok, worker} ->
-        worker
+        worker_ref = Process.monitor(worker)
+        {worker, worker_ref}
 
       thing ->
         IO.puts("ERROR")
@@ -243,7 +244,7 @@ defmodule Pooly.PoolServer do
     Supervisor.terminate_child(sup, pid)
   end
 
-  defp handle_worker_exit(_pid, state) do
+  defp handle_worker_exit(downed_pid, state) do
     %{
       worker_sup: worker_sup,
       workers: workers,
@@ -253,11 +254,14 @@ defmodule Pooly.PoolServer do
       imfa: imfa
     } = state
 
+    :ok = demonitor_worker(downed_pid, state)
+
     case :queue.out(waiting) do
       {{:value, {from, ref}}, left_waiting} ->
         new_worker = new_worker(worker_sup, imfa)
-        true = :ets.insert(monitors, {new_worker, ref})
-        GenServer.reply(from, new_worker)
+        {new_worker_pid, _worker_ref} = new_worker
+        true = :ets.insert(monitors, {new_worker_pid, ref})
+        GenServer.reply(from, new_worker_pid)
         %{state | waiting: left_waiting}
 
       {:empty, empty} when overflow > 0 ->
@@ -265,6 +269,16 @@ defmodule Pooly.PoolServer do
 
       {:empty, empty} ->
         %{state | waiting: empty, workers: [new_worker(worker_sup, imfa) | workers]}
+    end
+  end
+
+  defp demonitor_worker(worker_pid, %{workers: workers}) do
+    case workers |> Enum.filter(fn {pid, ref} -> pid == worker_pid end) do
+      [{_pid, ref}] ->
+        Process.demonitor(ref)
+        :ok
+      [] ->
+        :ok
     end
   end
 
--- pooly/lib/worker_supervisor.ex
+++ pooly/lib/worker_supervisor.ex
@@ -11,7 +11,7 @@ defmodule Pooly.WorkerSupervisor do
 
   def init([pool_server, {i, m, f, a}]) do
     Process.link(pool_server)
-    worker_opts = [restart: :permanent, shutdown: 5000, function: f]
+    worker_opts = [restart: :temporary, shutdown: 5000, function: f]
     children = [worker(m, a, worker_opts)]
 
     opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
